6548a006a7f64a4dc00a2bc52e6efb6add7254c1
Intikhab <islaminitkhab2gmail.com>
2023-06-20 13:12:20 +0530

    comit-changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import LiveSearch from './Components/LiveSearch';
+
+function App() {
+  return (
+    <LiveSearch/>
+  );
+}
+
+export default App;
